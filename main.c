0d4e93829c433de4147fafec939072c6480579fc
Clawwie <strongclawwhite@gmail.com>
2025-04-14 20:17:44 -0400

    Agregado chequeo de errores

--- main.c
+++ main.c
@@ -13,7 +13,33 @@ int main(int argc, char* argv[]) {
     char* palabra1 = argv[3];
     char* palabra2 = argv[4];
     char* archivo_de_salida = argv[5];
+    
+    ///FUNCIONES ERROR
+    ///1
+    int bruh = argc;
+    printf("%d\n", bruh);
+    if (bruh != 6){
+        printf("Te faltaron argumentos");
+        return 1;
+    }
 
+    ///2
+    if (!modos(modo)) {
+        printf("se ha producido un tremendo error, no pusiste un modo valido, recuerda que los modos validos son (encode) y (decode.\n");
+        return 1;
+    }
+    ///3
+    if (strlen(palabra1) != strlen(palabra2)){
+        printf("You should kill yourself NOW!! *rayos sfx*");
+        return 1;
+    }
+
+    ///4
+    if (strcmp(archivo_de_entrada, archivo_de_salida) == 0) {
+        printf("Errorazo, los archivos de entrada y salida tienen el mismo nombre (deben ser distintos).\n");
+        return 1;
+    }
+    ///////
 
     FILE* entrada = fopen(archivo_de_entrada, "r");
 
